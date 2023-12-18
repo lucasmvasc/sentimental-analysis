e83dfc3a1fb76c92f1801473995e5e1755913d61
Lucas Vasconcelos <lucas.vasc100@gmail.com>
2023-12-18 01:58:36 -0300

    Add - default keys

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
 
+# Última URL em uso
 API_URL = "https://8d29-187-18-138-207.ngrok-free.app/analyze"  
-API_KEY = "123456"
+API_KEY = "SUA_CHAVE_AQUI"
 
 audio_file = "audio/agendamento_de_reuniao.mp3"
 files = {"audio": open(audio_file, "rb")}
