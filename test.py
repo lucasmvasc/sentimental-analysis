34fbf61ce400b6606053bb163bdb89fd24b9d7e0
Lucas Vasconcelos <lucas.vasc100@gmail.com>
2023-12-18 00:18:38 -0300

    Delete - unused files

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 import requests
 
-API_URL = "https://8654-187-18-138-207.ngrok-free.app/analyze"  
+API_URL = "https://8d29-187-18-138-207.ngrok-free.app/analyze"  
 API_KEY = "123456"
 
 audio_file = "audio/agendamento_de_reuniao.mp3"
-
-data = {"conversation_id": "123"}
 files = {"audio": open(audio_file, "rb")}
 
 response = requests.post(
     API_URL,
     headers={"Api-Key": API_KEY},
-    data=data,
     files=files
 )
 
